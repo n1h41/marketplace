8404884fa21b272527d62e3db34c920c896ee976
nihal <nihal@innsof.com>
2024-06-26 14:12:22 +0530

    Completed admin login functionality

--- controllers/admin_controller.go
+++ controllers/admin_controller.go
@@ -1,33 +1,52 @@
-package handlers
+package controllers
 
 import (
+	"log"
+
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
 
 	"n1h41/marketplace/models"
+	"n1h41/marketplace/services"
 	"n1h41/marketplace/utils"
 	adminviews "n1h41/marketplace/views/admin_views"
 	"n1h41/marketplace/views/partials"
 )
 
-func GetAdminView(c *fiber.Ctx) error {
-	token := c.Cookies("token")
+type AdminController interface {
+	GetAdminView(c *fiber.Ctx) error
+	GetAdminSignInView(c *fiber.Ctx) error
+	HandleAdminLogin(c *fiber.Ctx) error
+}
+
+type adminController struct {
+	service services.AdminService
+}
+
+func AdminControllerConstructor(service services.AdminService) AdminController {
+	return &adminController{
+		service: service,
+	}
+}
+
+func (controller adminController) GetAdminView(c *fiber.Ctx) error {
+	/* token := c.Cookies("token")
 	if token == "" {
 		return c.Redirect("/admin/signin")
-	}
+	} */
 	adminView := adminviews.AdminView()
 	handler := adaptor.HTTPHandler(templ.Handler(adminView))
 	return handler(c)
 }
 
-func GetAdminSignInView(c *fiber.Ctx) error {
+func (controller adminController) GetAdminSignInView(c *fiber.Ctx) error {
 	signInView := adminviews.AdminSignInView()
 	handler := adaptor.HTTPHandler(templ.Handler(signInView))
 	return handler(c)
 }
 
-func HandleAdminLogin(c *fiber.Ctx) error {
+func (controller adminController) HandleAdminLogin(c *fiber.Ctx) error {
 	var errors map[string]string
 	errors = make(map[string]string)
 	var params models.AdminLoginModel
@@ -50,7 +69,13 @@ func HandleAdminLogin(c *fiber.Ctx) error {
 		return handler(c)
 	}
 
-	// TODO: call admin service to login user
+	if err := controller.service.Login(params); err != nil {
+		log.Println(err)
+		errors["loginError"] = err.Error()
+		signInComp := partials.SignIn(params, errors)
+		handler := adaptor.HTTPHandler(templ.Handler(signInComp))
+		return handler(c)
+	}
 
 	return c.Redirect("/admin")
 }
