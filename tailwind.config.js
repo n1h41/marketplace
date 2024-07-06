686218bfdecb55fade61dd826b39b8133a49f2fd
n1h41 <nihalninu25@gmail.com>
2024-07-06 15:53:00 +0530

    Created section to insert mulitple image url

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-"./views/**/*.{html,js,templ}",
-"./components/**/*.{html,js,templ}",
-],
-theme: {
-extend: {},
-},
-plugins: [require("@tailwindcss/forms")],
+  content: ["./views/**/*.{html,js,templ}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
