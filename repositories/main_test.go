d9a8a8c91e2a1b173c3398db56bdcc21dd14e2f9
n1h41 <nihalninu25@gmail.com>
2024-08-15 23:41:21 +0530

    Working on setup to test admin repo

---
+++ repositories/main_test.go
@@ -0,0 +1,112 @@
+package repositories
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://test:test@localhost:%s/marketplace_test_db?sslmode=disable"
+)
+
+var (
+	testDB   *sql.DB
+	pool     *dockertest.Pool
+	resource *dockertest.Resource
+	repo     AdminRepository
+)
+
+func TestMain(m *testing.M) {
+	// Set up the Docker test environment
+	setupDockerTestEnvironment()
+
+	// Apply database migrations
+	applyDatabaseMigrations()
+
+	repo = AdminRepoConstructor(testDB)
+	// Run the tests
+	code := m.Run()
+
+	// Tear down the Docker test environment
+	tearDownDockerTestEnvironment()
+
+	// Exit with the appropriate exit code
+	os.Exit(code)
+}
+
+func setupDockerTestEnvironment() {
+	setupTimeoutDuration := 2 * time.Minute
+	setupDone := make(chan bool)
+
+	go func() {
+		var err error
+		pool, err = dockertest.NewPool("")
+		if err != nil {
+			log.Fatalf("Could not construct pool: %s", err)
+		}
+
+		err = pool.Client.Ping()
+		if err != nil {
+			log.Fatalf("Could not connect to Docker: %s", err)
+		}
+
+		resource, err = pool.RunWithOptions(&dockertest.RunOptions{
+			Repository: "postgres",
+			Tag:        "12",
+			Env: []string{
+				"POSTGRES_USER=test",
+				"POSTGRES_PASSWORD=test",
+				"POSTGRES_DB=marketplace_test_db",
+				"listen_addresses = '*'",
+			},
+		}, func(config *docker.HostConfig) {
+			config.AutoRemove = true
+			config.RestartPolicy = docker.RestartPolicy{
+				Name: "no",
+			}
+		})
+		if err != nil {
+			log.Fatalf("Failed to create resource: %s", err)
+		}
+
+		if err := pool.Retry(func() error {
+			var err error
+			testDB, err = sql.Open(dbDriver, fmt.Sprintf(dbSource, resource.GetPort("5432/tcp")))
+			if err != nil {
+				return err
+			}
+
+			return testDB.Ping()
+		}); err != nil {
+			log.Fatalf("Could not connect to database: %s", err)
+		}
+
+		setupDone <- true
+	}()
+
+	select {
+	case <-setupDone:
+		log.Println("Docker test environment setup completed")
+	case <-time.After(setupTimeoutDuration):
+		log.Println("Docker test environment setup timed out")
+	}
+}
+
+func applyDatabaseMigrations() {
+	// panic("unimplemented")
+}
+
+func tearDownDockerTestEnvironment() {
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge Docker resource: %s", err)
+	}
+}
