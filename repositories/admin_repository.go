c17bfda394d033e7be280e090052336c00f7f878
Nihal Abdulla <141116046+nihal-innsof@users.noreply.github.com>
2024-08-16 19:16:05 +0530

    Created admin_repo method to create category, created associated (#8)
    entities and dto's

--- repositories/admin_repository.go
+++ repositories/admin_repository.go
@@ -5,25 +5,58 @@ import (
 	"errors"
 
 	"n1h41/marketplace/dto"
+	"n1h41/marketplace/entity"
 )
 
 type AdminRepository interface {
 	Login(dto.AdminLoginModel) error
 	CreateProduct(dto.AddProductModel) error
+	CreateCategory(dto.CreateNewCategory) error
 }
 
 type adminRepository struct {
 	db *sql.DB
 }
 
-// CreateProduct implements AdminRepository.
+func (r *adminRepository) getParentCategoryId(categoryName string) int {
+	var category entity.Category
+	query := "select * from categories where name = $1"
+	row, _ := r.db.Query(query, categoryName)
+	err := row.Scan(category)
+	if err != nil {
+		return 0
+	}
+	return category.Id
+}
+
+func (r *adminRepository) CreateCategory(param dto.CreateNewCategory) error {
+	var parentCategoryId int
+	var query string
+	var err error
+	if param.IsSubCategory {
+		parentCategoryId = r.getParentCategoryId(param.Parent)
+		query = "insert into categories(name, is_sub_category, parent) values($1, $2, $3)"
+		_, err = r.db.Exec(query, param.Name, param.IsSubCategory, parentCategoryId)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	query = "insert into categories(name, is_sub_category) values($1, $2)"
+	_, err = r.db.Exec(query, param.Name, param.IsSubCategory)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (r *adminRepository) CreateProduct(dto.AddProductModel) error {
 	// query := "select id from categories where name = $1"
 	panic("unimplemented")
 }
 
 func (r adminRepository) Login(params dto.AdminLoginModel) (err error) {
-	row := r.db.QueryRow("SELECT * FROM users WHERE email = $1 AND password = $2", params.Email, params.Password)
+	row := r.db.QueryRow("SELECT * FROM users WHERE email = $1 AND password = $2 AND role = 'Admin'", params.Email, params.Password)
 	if row.Err() != nil {
 		err = row.Err()
 		return
--- repositories/admin_repository_test.go
+++ repositories/admin_repository_test.go
@@ -1,6 +1,61 @@
 package repositories
 
-import "testing"
+import (
+	"testing"
 
-func TestCreateProduct(t *testing.T) {
+	"n1h41/marketplace/dto"
+	"n1h41/marketplace/entity"
+)
+
+func createUser(t *testing.T) {
+	user := entity.User{
+		FirstName: "Nihal",
+		LastName:  "Abdulla",
+		Email:     "test@gmail.com",
+		Password:  "test1234",
+		Role:      "Admin",
+	}
+
+	r, err := testDB.Exec("INSERT INTO users(email, password, first_name, last_name, role) values($1, $2, $3, $4, $5)", user.Email, user.Password, user.FirstName, user.LastName, user.Role)
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
+
+	t.Log(r)
+}
+
+func TestLogin(t *testing.T) {
+	createUser(t)
+
+	params := dto.AdminLoginModel{
+		Email:    "test@gmail.com",
+		Password: "test1234",
+	}
+
+	err := repo.Login(params)
+	if err != nil {
+		t.Fatalf("Failed: %s", err)
+	}
+}
+
+func TestCreateCategory(t *testing.T) {
+	newCategory := dto.CreateNewCategory{
+		Name:          "Food and Beverages",
+		IsSubCategory: false,
+	}
+	err := repo.CreateCategory(newCategory)
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
+
+	newCategory = dto.CreateNewCategory{
+		Name:          "Pepsi",
+		IsSubCategory: true,
+		Parent:        "Food and Beverages",
+	}
+
+	err = repo.CreateCategory(newCategory)
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
 }
